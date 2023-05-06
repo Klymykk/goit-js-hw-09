d811408cc5d67a5dade3a2950e82ca80cb90b677
Klymuk <114335106+Klymykk@users.noreply.github.com>
2023-05-06 14:41:37 +0300

    commit

--- src/js/01-color-switcher.js
+++ src/js/01-color-switcher.js
@@ -1,1 +1,20 @@
+const body = document.querySelector("body")
+const start = document.querySelector("[data-start]");
+const stop = document.querySelector("[data-stop]");
+
+function getRandomHexColor() {
+    return `#${Math.floor(Math.random() * 16777215).toString(16).padStart(6, 0)}`;
+}
+
+start.addEventListener("click", ()=>{
+    const onStartClick = setInterval(()=> {
+        body.style.backgroundColor=`${getRandomHexColor()}`
+    },1000);
+    start.disabled = true;
+
+    stop.addEventListener("click", () =>{
+        clearInterval(onStartClick);
+        start.disabled = false;
+    });
+});
 
