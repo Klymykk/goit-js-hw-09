1b6f7c46bfb23fc6bcef6f6f2684153ae962768f
Klymykk <114335106+Klymykk@users.noreply.github.com>
2023-05-07 18:31:51 +0000

    Deploying to gh-pages from @ Klymykk/goit-js-hw-09@a5a9a7e9df2d04869f96f32dd4dd85fb2414b464 🚀

--- 01-color-switcher.7fa4b957.js
+++
@@ -1,2 +0,0 @@
-!function(){var t=document.querySelector("body"),e=document.querySelector("[data-start]"),a=document.querySelector("[data-stop]");e.addEventListener("click",(function(){var n=setInterval((function(){t.style.backgroundColor="".concat("#".concat(Math.floor(16777215*Math.random()).toString(16).padStart(6,0)))}),1e3);e.disabled=!0,a.addEventListener("click",(function(){clearInterval(n),e.disabled=!1}))}))}();
-//# sourceMappingURL=01-color-switcher.7fa4b957.js.map
---
+++ 01-color-switcher.f833840a.js
@@ -0,0 +1,2 @@
+const e=document.querySelector("body"),t=document.querySelector("[data-start]"),d=document.querySelector("[data-stop]");t.addEventListener("click",(()=>{const a=setInterval((()=>{e.style.backgroundColor=`#${Math.floor(16777215*Math.random()).toString(16).padStart(6,0)}`}),1e3);t.disabled=!0,d.disabled=!1,d.addEventListener("click",(()=>{clearInterval(a),t.disabled=!1,d.disabled=!0}))}));
+//# sourceMappingURL=01-color-switcher.f833840a.js.map
